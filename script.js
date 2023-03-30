512c6dc42d5f1cfa13c60383c08bef04850e9a2b
João Neiva <jg.neiva@gmail.com>
2023-03-30 11:58:41 -0300

    pratica guiada laços II

--- script.js
+++ script.js
@@ -1,25 +1,29 @@
 // --------------- PRÁTICA GUIADA ---------------
 
+// Em uma escola, são aplicadas 4 provas por bimestre.
+// Tendo como base o array de cada bimestre, crie um programa que mostre a média e a soma das notas de cada bimestre.
+// Para esse programa, considere o seguinte formato:
+
+// const bimestre1 = [ 3,7,8,9 ]
+// const bimestre2 = [ 9,8,10,5]
+// const bimestre3 = [ 7,4,2,6]
+// const bimestre4 = [ 9,4,5,5 ]
+
+// const notasDoAno = [bimestre1, bimestre2, bimestre3, bimestre4]
+
+// for (let i=0; i<notasDoAno.length; i++){
+//   let soma = 0
+//   for (let j=0; j< notasDoAno[i].length; j++){
+//     soma = soma + notasDoAno[i][j]
+    
+//   }
+//   console.log(`A soma das notas do bimestre ${i+1} é ${soma}`)
+//   console.log(`A média do bimestre ${i+1} é ${soma/notasDoAno[i].length} `)
+  
+// }
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// --------------- EXERCÍCIO DE FIXAÇÃO ---------------
+// // --------------- EXERCÍCIO DE FIXAÇÃO ---------------
 const filmes = [
   {
     titulo: 'O Auto da Compadecida',
@@ -57,3 +61,35 @@ const filmes = [
 ]
 
 // escreva seu código abaixo 👇🏻
+// Utilizando o array de filmes que está no template, complete os exercícios abaixo:
+
+// Crie um laço for para exibir o título, o ano e a pessoa diretora dos filmes como uma string:
+// Exemplo de saída: “O Homem que copiava, de 2003, dirigido por Jorge Furtado”
+// b. Crie um laço for dentro do primeiro, para concatenar o elenco numa string:
+// 	Exemplo de saída:
+// “O Homem que copiava, de 2003, dirigido por Jorge Furtado”
+// “Ator/Atriz 1: Lázaro Ramos"
+// "Ator/Atriz 2: Leandra Leal"
+// "Ator/Atriz 3: Pedro Cardoso”
+
+for(let i = 0; i < filmes.length; i++){
+
+  console.log(`${filmes[i].titulo}, de ${filmes[i].ano}, dirigido por ${filmes[i].diretor}`)
+
+    for(let j = 0; j < filmes[i].elenco.length; j++){
+    console.log(filmes[i].elenco[j])
+    }
+    console.log("- - - - - - - - - - - - - - - - - - -")
+}
+
+// Ou simplificando com o for of e for in
+console.log("____________________________________")
+  
+for(let i in filmes){
+
+  console.log(`${filmes[i].titulo}, de ${filmes[i].ano}, dirigido por ${filmes[i].diretor}`)
+    for(j in filmes[i].elenco){
+    console.log(filmes[i].elenco[j])
+    }
+  console.log("- - - - - - - - - - - - - - - - - - -")
+}
